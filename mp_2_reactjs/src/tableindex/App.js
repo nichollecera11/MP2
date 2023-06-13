9ae7b5d7ff85da7c9215173d37cd6b82ca726505
Jan Alfonso <janmerissalfonso@gmail.com>
2023-06-13 15:21:19 +0800

    added table and modal

---
+++ mp_2_reactjs/src/tableindex/App.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+import "./tableIndex/table.css";
+import { Table } from "./components/Table";
+import { Modal } from "./components/Modal";
+
+function tableSlots(props) {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [rows, setRows] = useState([
+    {
+      floorSlot: "1 | 1",
+      carDescription: "",
+      status: "Open",
+    },
+  ]);
+  const [rowToEdit, setRowToEdit] = useState(null);
+
+  const handleDeleteRow = (targetIndex) => {
+    setRows(rows.filter((_, idx) => idx !== targetIndex));
+  };
+
+  const handleEditRow = (idx) => {
+    setRowToEdit(idx);
+
+    setModalOpen(true);
+  };
+
+  const handleSubmit = (newRow) => {
+    rowToEdit === null
+      ? setRows([...rows, newRow])
+      : setRows(
+          rows.map((currRow, idx) => {
+            if (idx !== rowToEdit) return currRow;
+
+            return newRow;
+          })
+        );
+  };
+
+  return (
+    <div className="slotsTable">
+      <Table rows={rows} deleteRow={handleDeleteRow} editRow={handleEditRow} />
+      <button onClick={() => setModalOpen(true)} className="btn">
+        Add
+      </button>
+      {modalOpen && (
+        <Modal
+          closeModal={() => {
+            setModalOpen(false);
+            setRowToEdit(null);
+          }}
+          onSubmit={handleSubmit}
+          defaultValue={rowToEdit !== null && rows[rowToEdit]}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
